1736000f27a2ef739df9d7b48622d4371332ab39
Milttreonna Owens <mowens@basecampcodingacademy.org>
2016-12-08 11:37:11 -0600

    fixed div problems

---
+++ madlib.js
@@ -0,0 +1,89 @@
+
+document.getElementById("story").style.visibility= "hidden";
+
+
+function submitit(){
+  var submit = document.getElementById("submit").checked;
+
+    var input1= document.getElementById("input1").value;
+    document.getElementById("one").innerHTML= input1 ;
+
+    var input2= document.getElementById("input2").value;
+    document.getElementById("two").innerHTML= input2 ;
+
+    var input3= document.getElementById("input3").value;
+    document.getElementById("three").innerHTML= input3 ;
+
+    var input4= document.getElementById("input4").value;
+    document.getElementById("four").innerHTML= input4 ;
+
+    var input5= document.getElementById("input5").value;
+    document.getElementById("five").innerHTML= input5 ;
+
+    var input6= document.getElementById("input6").value;
+    document.getElementById("six").innerHTML= input6 ;
+
+    var input7= document.getElementById("input7").value;
+    document.getElementById("seven").innerHTML= input7 ;
+
+    var input8= document.getElementById("input8").value;
+    document.getElementById("eight").innerHTML= input8 ;
+
+    var input9= document.getElementById("input9").value;
+    document.getElementById("nine").innerHTML= input9 ;
+
+    var input10= document.getElementById("input10").value;
+    document.getElementById("ten").innerHTML= input10 ;
+
+    var input11= document.getElementById("input11").value;
+    document.getElementById("eleven").innerHTML= input11 ;
+
+    var input12= document.getElementById("input12").value;
+    document.getElementById("twelve").innerHTML= input12 ;
+
+    var input13= document.getElementById("input13").value;
+    document.getElementById("13").innerHTML= input13 ;
+
+    var input14= document.getElementById("input14").value;
+    document.getElementById("14").innerHTML= input14 ;
+
+    var input15= document.getElementById("input15").value;
+    document.getElementById("15").innerHTML= input15 ;
+
+    var input16= document.getElementById("input16").value;
+    document.getElementById("16").innerHTML= input16 ;
+
+    var input17= document.getElementById("input17").value;
+    document.getElementById("17").innerHTML= input17 ;
+
+    var input18= document.getElementById("input18").value;
+    document.getElementById("18").innerHTML= input18 ;
+
+    var input19= document.getElementById("input19").value;
+    document.getElementById("19").innerHTML= input19 ;
+
+    var input20= document.getElementById("input20").value;
+    document.getElementById("20").innerHTML= input20 ;
+
+    var input21= document.getElementById("input21").value;
+    document.getElementById("21").innerHTML= input21 ;
+
+    var input22= document.getElementById("input22").value;
+    document.getElementById("22").innerHTML= input22 ;
+
+    var input23= document.getElementById("input23").value;
+    document.getElementById("23").innerHTML= input23 ;
+
+    var input24= document.getElementById("input24").value;
+    document.getElementById("24").innerHTML= input24 ;
+
+    var input25= document.getElementById("input25").value;
+    document.getElementById("25").innerHTML= input25 ;
+
+    var input26= document.getElementById("input26").value;
+    document.getElementById("26").innerHTML= input26 ;
+
+
+
+document.getElementById("story").style.visibility= "visible";
+}
