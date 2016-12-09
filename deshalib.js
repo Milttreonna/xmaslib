5fe96d1f009c769453e572657e041445b34bafad
Milttreonna Owens <mowens@basecampcodingacademy.org>
2016-12-09 15:28:59 -0600

    changing grandma css

--- deshalib.js
+++ deshalib.js
@@ -1,6 +1,5 @@
 $('document').ready(function() {
     $('#story').hide();
-    $('#down').hide();
     $('button').on('click', function() {
         var inputArray = $('form').find('input');
         for (var c = 0; c < inputArray.length; c++) {
@@ -8,8 +7,6 @@ $('document').ready(function() {
             var new_text = inputArray[c].value;
             $(new_class).text(new_text);
         }
-        // $("#inputs").hide(2000);
-        $('#down').show(2000);
-        $('#story').show(2000);
+        $('#story').show(300);
     });
 });
